dea62e3993f61e2b6e3aadfa1b3c7ff293ec4c93
yuvaraj <yuvaraj.r@eatwithus.in>
2016-04-05 17:32:17 +0530

    giving the correct url in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'yoursite.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'mysterious-lake-94922.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
@@ -87,7 +87,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: 'yoursite.herokuapp.com',
+    domain: 'mysterious-lake-94922.herokuapp.com',
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: "thegreatyuvaraj14@gmail.com",
