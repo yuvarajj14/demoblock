5116502aced1797e82297c7ea9b1fa69fe9410d5
yuvaraj <yuvaraj.r@eatwithus.in>
2016-04-05 17:41:35 +0530

    removed the registerable in admin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,14 +81,14 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
     domain: 'mysterious-lake-94922.herokuapp.com',
-    authentication: "plain",
+    :authentication => :plain,
     enable_starttls_auto: true,
     user_name: "thegreatyuvaraj14@gmail.com",
     password: "8344330049"
