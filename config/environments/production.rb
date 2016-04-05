34ec87ae5d474dbc0483af33429b3d226aa151b9
yuvaraj <yuvaraj.r@eatwithus.in>
2016-04-05 18:09:42 +0530

    added something  in production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,22 +79,18 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => 'mysterious-lake-94922.herokuapp.com' }
 
-  Rails.application.routes.default_url_options[:host] = 'mysterious-lake-94922.herokuapp.com'
-
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "gmail.com",
-    :authentication => :login,
+    domain: 'mysterious-lake-94922.herokuapp.com',
+    authentication: "plain",
     enable_starttls_auto: true,
     user_name: "thegreatyuvaraj14@gmail.com",
     password: "8344330049"
     }
 end
-
-
